f82f6b5e709f623189bc571eafc20cde316e4195
Simon Flueckiger <si.flueckiger@mail.com>
2019-05-07 12:09:30 +0200

    button is working properly

--- gifchiste/tests/button.py
+++ gifchiste/tests/button.py
@@ -1,19 +1,26 @@
+#!/usr/bin/env python
+
 import RPi.GPIO as GPIO
-import time
+from time import sleep
+import atexit
 
+#Variables to change as needed
+btn_pin = 24 # pin for the button
+debounce = 0.5 # how long to debounce the button. Add more time if the button triggers too many times.
+
+#GPIO setup
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(btn_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-buttonPin = 24
+def cleanup():
+  print('Goodbye.')
+  GPIO.cleanup()
+atexit.register(cleanup)
 
-GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+print('Push Button')
+print('Press Ctrl+C to exit')
 
-raw_input("Press Enter when ready\n>")
-
-try:
-	GPIO.wait_for_edge(buttonPin, GPIO.FALLING)
-	print("BUTTON PRESSED")
-
-except keyboardInterrupt:
-	GPIO.cleanup()
-GPIO.cleanup()
-
+while True:
+	GPIO.wait_for_edge(btn_pin, GPIO.FALLING)
+	sleep(debounce)
+	print("Button pressed")
